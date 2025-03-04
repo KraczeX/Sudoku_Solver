e3f0cf8212fe0f227b835f6b765d2e4ded1405bf
Krecik <bartoszkraczek2006@gmail.com>
2025-03-05 00:13:03 +0100

    Initial commit

---
+++ sudoku.c
@@ -0,0 +1,12 @@
+#include "sudoku.h"
+
+int main()
+{
+    int ** puzzle;
+    Square *** sudoku;
+    puzzle = createPuzzle();
+
+    printPuzzle(puzzle);
+    sudoku = setUpPuzzle(puzzle);
+    return 0;
+}
---
+++ sudoku.h
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+char POSSIBLE = 0x1FF;
+
+typedef struct Box
+{
+    /* data */
+} Box;
+
+typedef struct Square
+{
+    int number;
+    char code;
+
+    Box * box;
+    int row;
+    int column;    
+} Square;
+
+int ** createPuzzle();
+void printPuzzle(int ** puzzle);
+Square *** setUpPuzzle(int ** puzzle);

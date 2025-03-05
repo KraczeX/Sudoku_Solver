c0e4e088f4760de63bd41b9980d5eca44def5549
Krecik <bartoszkraczek2006@gmail.com>
2025-03-05 20:42:06 +0100

    update

--- src/puzzle.c
+++ src/puzzle.c
@@ -7,11 +7,11 @@ Square *** setUpPuzzle(int ** puzzle)
 
     sudoku = (Square***)malloc(sizeof(Square**)*9);
 
-        for(i=0; i<9; i++)
+        for(i=0; i<SIZE_ROWS; i++)
         {   
             sudoku[i] = (Square**)malloc(sizeof(Square*)*9);
 
-            for(j=0; j<9; j++)
+            for(j=0; j<SIZE_COLUMNS; j++)
             {
                 sudoku[i][j] = (Square*)malloc(sizeof(Square)*9);
                 sudoku[i][j] ->number =  puzzle[i][j];
@@ -66,11 +66,11 @@ void printPuzzle(int ** puzzle)
     int i, j;
 
     printf("------------------------------\n");
-    for(i = 0; i < 9; i++)
+    for(i = 0; i < SIZE_ROWS; i++)
     {
         printf("|");
         // print each row
-        for(j = 0; j < 9; j++)
+        for(j = 0; j < SIZE_COLUMNS; j++)
         {
             printf(" %d ", puzzle[i][j]);
             if (((j + 1) % 3) ==0)
